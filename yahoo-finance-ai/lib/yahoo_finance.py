44e9b72b71c13194d192def9adcca5bd68469e1d
Sayeeswar <sayeeswar2004@gmail.com>
2026-09-03 15:14:16 +0530

    Added calendarEvents module in  yfinance v2--> added : after if statmentv2

--- yahoo-finance-ai/lib/yahoo_finance.py
+++ yahoo-finance-ai/lib/yahoo_finance.py
@@ -344,7 +344,7 @@ def execute_yahoo_intent(intent: dict):
     elif submodule == "sec_filings":
         return make_json_safe(stock.sec_filings)
     elif submodule == "calemdarEvents":
-         return make_json_safe(stock.calendarEvents)
+         return make_json_safe(stock.Calendars)
 
     else:
         raise ValueError(f"Unsupported Yahoo Finance submodule: {submodule}")
